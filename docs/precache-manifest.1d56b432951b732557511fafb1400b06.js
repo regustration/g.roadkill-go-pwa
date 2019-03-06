d7e1835374e73d260e47fa2ce83ad1eb8d8a0ce5
Gus <regustration@gmail.com>
2019-03-06 20:23:45 +0800

    add docs

---
+++ docs/precache-manifest.1d56b432951b732557511fafb1400b06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a844b09756a558833274",
+    "url": "/js/chunk-vendors.22ea2a72.js"
+  },
+  {
+    "revision": "7f15b6cce578f25102cd",
+    "url": "/js/app.1b09c543.js"
+  },
+  {
+    "revision": "48af24cd7dae7280ac56",
+    "url": "/js/about.41d636ee.js"
+  },
+  {
+    "revision": "2090735341b8074b706ace326d583ca9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a844b09756a558833274",
+    "url": "/css/chunk-vendors.2c7f3eba.css"
+  },
+  {
+    "revision": "7f15b6cce578f25102cd",
+    "url": "/css/app.96bf15fe.css"
+  }
+];
