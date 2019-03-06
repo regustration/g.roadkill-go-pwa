79513f5d4106f1f6f405ca419dda5c1c92f66276
Gus <regustration@gmail.com>
2019-03-06 17:31:33 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './registerServiceWorker'
+
+import { LMap, LTileLayer, LMarker, LPopup, LControl } from 'vue2-leaflet'
+import { Icon } from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+Vue.component('l-map', LMap)
+Vue.component('l-tile-layer', LTileLayer)
+Vue.component('l-marker', LMarker)
+Vue.component('l-popup', LPopup)
+Vue.component('l-control', LControl)
+
+// this part resolve an issue where the markers would not appear
+delete Icon.Default.prototype._getIconUrl
+
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+})
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
